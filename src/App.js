d6b7eb1777ff522d5c86943586e535b094d7b6c2
Ghulam Subbhani <gsmalik030@gmail.com>
2023-03-21 14:45:25 +0200

    Created Hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world
-      </h1>
+      <Router>
+        <Routes>
+        <Route path='/' element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
